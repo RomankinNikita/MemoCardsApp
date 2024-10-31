8897e85ba18e1e9aac44b03a1eb30fd6d12864de
Nikita Romankin <nikita.romankin@karuna.group>
2024-11-01 00:26:50 +0400

    router fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { RouterProvider, createRouter, createMemoryHistory } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
 import "./index.css";
 
-const router = createRouter({ routeTree });
+const memoryHistory = createMemoryHistory({
+  initialEntries: ['/'],
+})
+
+const router = createRouter({ routeTree, history: memoryHistory });
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
